de8539daa59068fb16ffe8705fe00cfcdab179d1
TrapFish <92753280+TrapFish@users.noreply.github.com>
2024-12-28 19:03:03 +0530

    Fixed key properties and added fix for the className

--- src/component/DataRepresentation/DataRepresent.jsx
+++ src/component/DataRepresentation/DataRepresent.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import {CORE_CONCEPTS} from './data';
+import { CORE_CONCEPTS } from './data';
 import DisplayCard from '../../modules/DisplayCard/DisplayCard';
 
-export default function DataRepresent(){
-    return (
-        <>
-          {CORE_CONCEPTS.map((value, index)=> {
-            return (
-                <>
-                  <DisplayCard {...value}/>
-                </>
-            )
-          })}
-        </>
-    )
+export default function DataRepresent() {
+  return (
+    <>
+      {CORE_CONCEPTS.map((value, index) => {
+        return (
+
+          <DisplayCard key={value?.title} {...value} />
+
+        )
+      })}
+    </>
+  )
 }
--- src/modules/DisplayCard/DisplayCard.jsx
+++ src/modules/DisplayCard/DisplayCard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function DisplayCard({ title, image, description }) {
     return (
         <>
-            <div class="card-data">
+            <div className="card-data">
                 <img src={image} alt={title} className='image-alt'/>
-                    <div class="container">
+                    <div className="container">
                         <h4><b>{title}</b></h4>
                         <p>{description}</p>
                     </div>
